ca89ea26956dd19fb24bcb6e9b9b3db35ba081af
nashi <nashibamalik2005@gmail.com>
2026-04-17 12:10:57 +0530

    hello

---
+++ Temp.java
@@ -0,0 +1,9 @@
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+}
